08ed7c675c84a7a1f44c84401b50fa89224cb5cc
cendelinova <pcendelinova@seznam.cz>
2016-06-22 21:53:02 +0200

    Oprava Level 3
    Odstraněna chyba jstl, nyní funkční verze

--- my-app/src/main/java/com/mycompany/web/HelloIbaServlet.java
+++ my-app/src/main/java/com/mycompany/web/HelloIbaServlet.java
@@ -3,10 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycompany.servlets;
+package com.mycompany.web;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -21,6 +23,7 @@ import javax.servlet.http.HttpServletResponse;
 public class HelloIbaServlet extends HttpServlet {
 
     public static final String URL_MAPPING = "/servlet";
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
@@ -33,7 +36,7 @@ public class HelloIbaServlet extends HttpServlet {
             }
         }
         req.setAttribute("x", x);
-        req.getRequestDispatcher("/index.jsp").forward(req, resp);
+        req.getRequestDispatcher("/WEB-INF/index.jsp").forward(req, resp);
     }
 
 }
