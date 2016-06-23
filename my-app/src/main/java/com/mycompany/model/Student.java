6f1c1b34a960a345276b7cc594455049ccede94b
cendelinova <pcendelinova@seznam.cz>
2016-06-23 22:10:31 +0200

    Level 6 - Validace
    zatim nefunkcni verze

--- my-app/src/main/java/com/mycompany/model/Student.java
+++ my-app/src/main/java/com/mycompany/model/Student.java
@@ -7,6 +7,10 @@ package com.mycompany.model;
 
 import java.util.Date;
 import java.util.Objects;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Pattern;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotEmpty;
 import org.springframework.format.annotation.DateTimeFormat;
 
 /**
@@ -14,8 +18,20 @@ import org.springframework.format.annotation.DateTimeFormat;
  * @author Petra
  */
 public class Student {
+
+    @Pattern(regexp = "[a-zA-Z]+", message = "The field must contain only characters.")
+    @Length(min = 1, max = 60, message = "This field must be between 1 and 60 characters.")
+    @NotEmpty(message = "This field is required.")
     private String name;
+
+    
+    @Pattern(regexp = "[a-zA-Z]+", message = "The field must contain only characters.")
+    @Length(min = 1, max = 60, message = "This field must be between 1 and 60 characters.")
+    @NotEmpty(message = "This field is required.")
     private String surname;
+    
+    @Past(message = "There’s a birthday field which cannot be a future date.")
+    @NotEmpty(message = "This field is required.")
     @DateTimeFormat(pattern = "dd.MM.yyyy")
     private Date birthday;
     private Gender gender;
@@ -98,7 +114,5 @@ public class Student {
         }
         return true;
     }
-    
-    
-            
+
 }
--- my-app/src/main/java/com/mycompany/web/StudentController.java
+++ my-app/src/main/java/com/mycompany/web/StudentController.java
@@ -5,10 +5,11 @@
  */
 package com.mycompany.web;
 
-import com.mycompany.model.Gender;
 import com.mycompany.model.Student;
+import javax.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -23,16 +24,18 @@ public class StudentController {
     @RequestMapping(value = "/student/add", method = RequestMethod.GET)
     public String showForm(ModelMap model) {
         model.addAttribute("studentForm", new Student());
-        //model.addAttribute("gender", Gender.values());
         return "add";
     }
 
     @RequestMapping(value = "/student/result", method = RequestMethod.POST)
-    public String addStudent(@ModelAttribute("studentForm") Student student,
-            ModelMap model) {
-        model.addAttribute("message","Student added successfully");
-        model.addAttribute("student",student);
-       
+    public String addStudent(@ModelAttribute("studentForm") @Valid Student student,
+            ModelMap model, BindingResult result) {
+        if (result.hasErrors()) {
+            return "add";
+        }
+        model.addAttribute("message", "Student added successfully");
+        model.addAttribute("student", student);
         return "result";
+
     }
 }
